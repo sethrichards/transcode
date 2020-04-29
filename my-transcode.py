1b2fa73d4bd01b2e3ca1359ab735a83320c9163b
Seth Richards <seth@sethrichards.com>
2020-04-29 16:20:23 -0400

    Do proper argument parsing for framerate, crop, and deinterlace options.

--- my-transcode.py
+++ my-transcode.py
@@ -4,14 +4,64 @@ import sys
 import os
 import subprocess
 import termcolor
+import argparse
 
-infile = os.path.abspath(sys.argv[1])
-outfile = os.path.abspath(sys.argv[2])
+argParser = argparse.ArgumentParser(description='Frontend for transcode-video')
+argParser.add_argument('-f', '--framerate',
+                       help='One of [film|ntsc|pal].')
+argParser.add_argument('-c', '--crop',
+                       help='0 for no crop, otherwise output from "detect-crop". Defaults to "detect".')
+argParser.add_argument('--deinterlace', action="store_true",
+                       help='Pass --filter=deinterlace to Handbrake. Works better for live action.')
+argParser.add_argument('--detelecine', action="store_true",
+                       help='Pass --filter=detelecine to Handbrake. Works better for animation.')
+argParser.add_argument('infile')
+argParser.add_argument('outfile')
 
-crop_arg = "--crop detect"
-frame_arg = "--force-rate 23.976"
-other_arg = "--add-subtitle eng --filter detelecine"
+args = argParser.parse_args()
 
+# Always preserve English subtitles
+other_arg = "--add-subtitle eng"
+
+# Handle framerate argument
+if args.framerate:
+    def getFramerate(rate):
+        frameNum = {
+            "film": "23.976",
+            "pal" : "25",
+            "PAL" : "25",
+            "ntsc": "29.97",
+            "NTSC": "29.97",
+        }
+        #print frameNum.get(rate)
+        return frameNum.get(rate)
+    frame_arg = "--force-rate %s" % (getFramerate(args.framerate))
+else:
+    frame_arg = ""
+
+# Handle cropping argument
+if args.crop:
+    if args.crop == "0":
+        crop_arg = "--crop 0:0:0:0"
+    else:
+        crop_arg = "--crop %s" % (args.crop)
+else:
+    crop_arg = "--crop detect"
+
+# Deinterlacing etc
+if args.deinterlace:
+    other_arg += " --filter deinterlace"
+
+if args.detelecine:
+    other_arg += " --filter detelecine"
+
+#sys.exit(0)
+
+# Filenames should be left over
+infile = os.path.abspath(args.infile)
+outfile = os.path.abspath(args.outfile)
+
+# Do the actual transcoding!
 print termcolor.colored('=========================== Starting Transcode ===========================', 'cyan')
 
 if infile.endswith('.mkv'):
@@ -37,4 +87,3 @@ if infile.endswith('.mkv'):
 else:
     print termcolor.colored('Input file not an MKV!', 'red')
     sys.exit(-1)
-    
