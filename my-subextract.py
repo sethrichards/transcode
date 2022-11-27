e6d6a9754e02d66850b2f45654c1310220876840
Seth Richards <sethrichards@gmail.com>
2022-11-27 06:47:39 -0500

    subextract fixes

--- my-subextract.py
+++ my-subextract.py
@@ -5,32 +5,41 @@ import argparse
 import subprocess
 
 argParser = argparse.ArgumentParser()
+argParser.add_argument('-n', '--dry-run', action='store_true',
+                       help='Dry run, don\'t actually call other-transcode')
+argParser.add_argument('-v', '--verbose', action='store_true',
+                       help='Enable verbose debug')
 argParser.add_argument('inputFile')
 argParser.add_argument('outputFile')
-arguments = argParser.parse_args()
+args = argParser.parse_args()
 
-inputFile = arguments.inputFile
-outputFile = arguments.outputFile
+inputFile = args.inputFile
+outputFile = args.outputFile
 
-trackListCmd = ['mkvmerge',  '-i', '-F', 'verbose-text', inputFile]
+trackListCmd = ['mkvmerge',  '-i', '-F', 'text', inputFile]
 trackExtractCmd = 'mkvextract'
 
 trackListProc = subprocess.Popen(trackListCmd, stdout=subprocess.PIPE)
-#print(subprocess.list2cmdline(trackListProc.args))
+if args.verbose:
+    print(subprocess.list2cmdline(trackListProc.args))
 rawTracks = trackListProc.communicate()[0].decode('utf-8').split('\n')
 
-#print (rawTracks)
+if args.verbose:
+    print ('Raw Tracks:', rawTracks)
 
 subsFound = 0
 for line in rawTracks:
     if(line.find('SubRip') != -1):
-        #print(line)
+        if args.verbose:
+            print(line)
         subsFound += 1
         trackNum = line.split(" ")[2].replace(':','')
         print ('Found SRT subtitles at track ' + trackNum + ', extracting with mkvextract...')
         extractCmd = [trackExtractCmd, inputFile, 'tracks', trackNum + ':' + outputFile]
-        #print(subprocess.list2cmdline(extractCmd))
-        extractProc = subprocess.Popen(extractCmd)
+        if args.verbose:
+            print('Extract command line', subprocess.list2cmdline(extractCmd))
+        if (args.dry_run == False):
+            extractProc = subprocess.Popen(extractCmd)
 
 if (subsFound == 0):
     print ("No text subtitles found in file " + inputFile)
